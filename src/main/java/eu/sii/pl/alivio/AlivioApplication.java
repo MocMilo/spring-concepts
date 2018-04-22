e86aa757f515e671fb02fb81e6b60960f4679a47
mmociewicz <mmociewicz@pl.sii.eu>
2018-04-22 08:03:01 +0200

    [spring-concepts] Added project base to repository.

---
+++ src/main/java/eu/sii/pl/alivio/AlivioApplication.java
@@ -0,0 +1,12 @@
+package eu.sii.pl.alivio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlivioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlivioApplication.class, args);
+    }
+}
