92e03e5b708b7b8b294fd85802844b0debacc7a7
mmociewicz <mmociewicz@pl.sii.eu>
2018-04-25 22:11:26 +0200

    Added more test items to repository.

--- src/main/java/eu/sii/pl/api/ApplicationConfiguration.java
+++ src/main/java/eu/sii/pl/api/ApplicationConfiguration.java
@@ -12,6 +12,7 @@ import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -40,7 +41,50 @@ public class ApplicationConfiguration {
                                         new CreditCard(new Long("1"),"4539128673131380", "3234","Visa", "Joe","Doe",
                                                 LocalDate.of(2020, 10, 31)))
                         ))));
-       List<Debtor> debtors = Arrays.asList(debtor1, debtor2);
+
+
+        CreditCard creditCard = new CreditCard(
+                new Long(1),
+                "4435",
+                "23424234224",
+                "VISA",
+                "John",
+                "Doe",
+                LocalDate.of(2020, 10, 11));
+
+        Payment payment = new Payment(2L, LocalDate.of(2018, 10, 10), new BigDecimal("124.0")
+                , creditCard);
+
+        Payment payment2 = new Payment(3L, LocalDate.of(2018, 3, 10), new BigDecimal("100.0")
+                , creditCard);
+        Payment payment3 = new Payment(4L, LocalDate.of(2019, 4, 11), new BigDecimal("120.0")
+                , creditCard);
+        Payment payment4 = new Payment(5L, LocalDate.of(2017, 3, 9), new BigDecimal("130.0")
+                , creditCard);
+        Payment payment5 = new Payment(6L, LocalDate.of(2016, 7, 1), new BigDecimal("90.0")
+                , creditCard);
+
+        List<Payment> paymentList = Arrays.asList(payment, payment2, payment3);
+        List<Payment> paymentList2 = Arrays.asList(payment4, payment3, payment5);
+
+        Debt debt = new Debt(new Long("1"), new BigDecimal("150"),
+                LocalDate.of(2017, 11, 13),
+                paymentList);
+
+        Debt debt2 = new Debt(2L, new BigDecimal("160"),
+                LocalDate.of(2017, 10, 13),
+                Collections.emptyList());
+        Debt debt3 = new Debt(3L, new BigDecimal("130"),
+                LocalDate.of(2017, 9, 13),
+                paymentList2);
+
+        Debtor debtor3 = new Debtor(1L,
+                "John",
+                "Rambo",
+                "9999-999-999",
+                Arrays.asList(debt, debt2, debt3));
+
+       List<Debtor> debtors = Arrays.asList(debtor1, debtor2, debtor3);
        repository.setDebtors(debtors);
     }
 }
